fbd57e212da3e629223d57bb32a60c496b4f4c86
Will <sypheren@gmail.com>
2022-04-09 19:12:19 -0500

    Files generated by PkgTemplates

---
+++ src/SorptionModels.jl
@@ -0,0 +1,5 @@
+module SorptionModels
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SorptionModels
+using Test
+
+@testset "SorptionModels.jl" begin
+    # Write your tests here.
+end
