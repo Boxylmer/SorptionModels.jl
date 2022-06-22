8614c7884a59bf3a81f767acf82bd6c66b48cac8
Will <sypheren@gmail.com>
2022-06-22 18:37:48 -0500

    setup for zm analysis

--- src/ModelAnalyses/ZimmLundberg.jl
+++ src/ModelAnalyses/ZimmLundberg.jl
@@ -1,16 +1,17 @@
-struct ZimmLundbergAnalysis{}
+struct ZimmLundbergAnalysis{AT, AOPDT, AOPDT, CFT, ACST}
     activities::AT
+    vol_fracs::VFT
+    a_over_phi_values::AOPDT
+    a_over_phi_derivatives::AOPDT
     cluster_functions::CFT
     average_cluster_size::ACST
-
-
 end
 
 function ZimmLundbergAnalysis(sorption_model::GABModel, activities, pen_molar_volume)
     # assumptions
     function get_volfrac(activity)
         conc = predict_concentration(sorption_model, activity)  # cc/cc
-        pen_volume = conc * pen_molar_volume / MembraneBase.CC_PER_MOL_STP  # ccstp/ccpol * cm3/mol / (ccstp/mol) = (cm3/mol * mol)/ccpol = cm3/pol
+        pen_volume = conc * pen_molar_volume / MembraneBase.CC_PER_MOL_STP  # ccstp/ccpol * cm3/mol / (ccstp/mol) = (cm3/mol * mol)/ccpol = cm3/ccpol = unitless
         vol_frac = pen_volume / (1 + pen_volume)  # unitless
         return vol_frac
     end
@@ -21,11 +22,13 @@ function ZimmLundbergAnalysis(sorption_model::GABModel, activities, pen_molar_vo
     end
     
     vol_fracs = get_volfrac.(activities)
-
-    @show d_aoverφ_d_a_vals = ForwardDiff.derivative.(act_per_volfrac, activities)
+    aoverφ_vals = act_per_volfrac.(activities)
+    d_aoverφ_d_a_vals = ForwardDiff.derivative.(act_per_volfrac, activities)
 
     # the clustering function is G_11 / molar volume
-    @show cluster_functions = (vol_fracs .- 1) .* d_aoverφ_d_a_vals .- 1
-    @show average_cluster_sizes = cluster_functions .* vol_fracs .+ 1
-    return
+    cluster_functions = (vol_fracs .- 1) .* d_aoverφ_d_a_vals .- 1
+    average_cluster_sizes = cluster_functions .* vol_fracs .+ 1
+
+    analysis = ZimmLundbergAnalysis(activities, vol_fracs, aoverφ_vals, d_aoverφ_d_a_vals, cluster_functions, average_cluster_sizes)
+    return analysis
 end
