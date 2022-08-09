01fff9d27b89917a62fa1973534660acfae8db35
Will <carboxylmer@gmail.com>
2022-08-09 16:20:08 -0500

    edits to nelf diagnostic search

--- src/ModelDiagnostics/NELFDiagnostics.jl
+++ src/ModelDiagnostics/NELFDiagnostics.jl
@@ -7,8 +7,6 @@
 function nelf_characteristic_parameter_error_map(isotherms, bulk_phase_char_params, added_text = ""; 
     pstars=200:30:1200, tstars=200:30:1200, rhostars=missing, verbose=true, data_only=true)
     
-    minimum_rhostar = maximum(polymer_density.(isotherms))
-    
     if ismissing(rhostars)
         rhostars = maximum(polymer_density.(isotherms)) * 1.2
     end
@@ -33,23 +31,15 @@ function nelf_characteristic_parameter_error_map(isotherms, bulk_phase_char_para
     end
 
     if typeof(rhostars) <: Number
-        
         errs_2D = errs_3D[:, :, 1]
         err_2d_heatmap = contourf(
             tstars, pstars, errs_2D[:, :, 1], 
             title="rho="*string(round(rhostars[1]; digits=2))*"g/cm3" * " " * added_text, 
             xlabel = "T* (K)", ylabel = "P* (MPa)", size=(600,600), clims=(minimum(errs_2D), sum(errs_2D)/length(errs_2D)))
-
-
         if data_only; return errs_2D; end
         return err_2d_heatmap
 
-
-
     elseif typeof(rhostars) <: AbstractRange
-
-        # removed factored code
-
         minlocation = argmin(errs_3D)
         @show global_min = [tstars[minlocation[2]]], [pstars[minlocation[1]]]
         if verbose
@@ -59,18 +49,17 @@ function nelf_characteristic_parameter_error_map(isotherms, bulk_phase_char_para
         mintstarvals = [tstars[frameminlocations[i][2]] for i in eachindex(rhostars)]
         minpstarvals = [pstars[frameminlocations[i][1]] for i in eachindex(rhostars)]
     
-
         if data_only
             return errs_3D, minlocation, frameminlocations
+        else
+            anim = @animate for i ∈ 1:size(errs_3D)[3]
+                myplot = contourf(tstars, pstars, errs_3D[:, :, i], title="rho="*string(round(rhostars[i]; digits=3))*"g/cm3",clim=(minimum(errs_3D),5), xlabel = "T* (K)", ylabel = "P* (MPa)")
+                annotate!(myplot, (0.8, 0.9), "min =" * string(round(minimum(errs_3D[:, :, i]); digits=3)))
+                scatter!(myplot, [mintstarvals], [minpstarvals], legend=false)
+                scatter!(myplot, global_min..., legend=false, markersize=6)
+            end
+            return anim 
         end
-
-        anim = @animate for i ∈ 1:size(errs_3D)[3]
-            myplot = contourf(tstars, pstars, errs_3D[:, :, i], title="rho="*string(round(rhostars[i]; digits=3))*"g/cm3",clim=(minimum(errs_3D),5), xlabel = "T* (K)", ylabel = "P* (MPa)")
-            annotate!(myplot, (0.8, 0.9), "min =" * string(round(minimum(errs_3D[:, :, i]); digits=3)))
-            scatter!(myplot, [mintstarvals], [minpstarvals], legend=false)
-            scatter!(myplot, global_min..., legend=false, markersize=6)
-        end
-        return anim
     end
    
 end
